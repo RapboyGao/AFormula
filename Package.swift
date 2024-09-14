777eab88d16c4455060baa702331ae6fe785ff26
RapboyGao <atgao74201@qq.com>
2024-09-14 10:19:16 +0800

    2024.09.14

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AFormula",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AFormula",
+            targets: ["AFormula"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/RapboyGao/AValue.git", branch: "main"),
+        .package(url: "https://github.com/RapboyGao/AUnit.git", branch: "main"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AFormula",
+            dependencies: [
+                .product(name: "AValue", package: "AValue"),
+                .product(name: "AUnit", package: "AUnit"),
+            ]),
+        .testTarget(
+            name: "AFormulaTests",
+            dependencies: ["AFormula"]),
+    ])
